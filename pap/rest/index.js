8796d94cd642c1a76c04f697b9ec86d77dccc186
dschreckling <dschrecking@users.noreply.github.com>
2017-05-10 08:56:35 +0200

    Adjust init to read correct settings and remove bug in get function

--- pap/rest/index.js
+++ pap/rest/index.js
@@ -8,12 +8,12 @@ var api = require("../api");
 
 function init(settings, app) {
     var p = path.sep;
-    if(settings.server && settings.server.path) {
-        p = settings.server.path;
+    if(settings && settings.path) {
+        p = settings.path;
         if(!p.endsWith(path.sep))
            p += path.sep;
     }
-
+    app.get(p, version);
     app.get(p + "policy" + path.sep, get);
     app.put(p + "policy" + path.sep, set);
     app.delete(p + "policy" + path.sep, del);
@@ -21,10 +21,13 @@ function init(settings, app) {
     return Promise.resolve();
 }
 
-function get(req, res) {
-    var id = req.body.id;
-    var property = req.body.property;
+function version(req, res) {
+    res.status(200).json({version: 1}).end();
+}
 
+function get(req, res) {
+    var id = req.query.id;
+    var property = req.query.property;
     api.get(id, property).then(function(p) {
         res.status(200).json(p).end();
     }, function(e) {
