59ae58d17beb1a8a0658d761de2ee3ef0cd06b85
dschreckling <dschrecking@users.noreply.github.com>
2017-05-10 08:53:19 +0200

    Server mode changes
    - disable API when in server mode
    - move ULock initialization to single threads

--- pap/index.js
+++ pap/index.js
@@ -1,82 +1,72 @@
 var clone = require('clone');
 var w = require('winston');
-
 w.level = process.env.LOG_LEVEL;
 
+var Promise = require('bluebird');
+
+var ULocks = require('ULocks');
 
 var Storage = require('./storage');
-
 var api = require('./api');
 
-var settings = {};
 var server = null;
 
-var defaultSettings = {
-    storage: {
-        type: "mongodb",
-        host: "localhost",
-        port: 27017,
-        password: "",
-        user: "",
-        dbName: "pap-database",
-        collection: "policies",
+function disabledAPI() {
+    var msg = "Usage error. API request ignored. PAP cannot answer direct API request while running in server mode!";
+    w.warn(msg);
+    
+    return Promise.reject(new Error(msg));
+};
 
-        // specifies whether the module should check
-        // the cache to fetch a policy, of course,
-        // this may induce additional lookups but on
-        // average using the cache is recommended
-        cache: {
-            enabled: false,
-            TTL: 600,
-            pubsub: {
-                type: "redis",
-                channel: "policyUpdates"
-            }
-        }
-    }
-}
-
-function getServerInit(userSettings, server, Rest) {
+function getServerInit(settings, server, Rest) {
     return function() {
         return new Promise(function(resolve, reject) {
-            Storage.init(userSettings.storage, userSettings.server.cluster).then(function() {
-                console.log("BEFORE API INIT ("+process.pid+")");
-                api.init(userSettings, Storage);
-                Rest.init(userSettings.server, server.app).then(function() {
-                    resolve();
-                }, function() {
-                    reject("ERROR: Unable to initialize REST interface");
-                })
-            }, function(e) {
-                w.error("Unable to initialize storage module");
-                reject(e);
+            Storage.init(settings.pap.storage, settings.pap.server.cluster).then(function() {
+                ULocks.init(settings.ulocks).then(function() {
+                    api.init(settings, Storage);
+                    Rest.init(settings.pap.server, server.app).then(function() {
+                        resolve();
+                    }, function() {
+                        reject("Unable to initialize REST interface");
+                    })
+                }, function(e) {
+                    w.error("Unable to initialize storage module");
+                    reject(e);
+                });
             });
         });
     };
 }
 
-function init(userSettings) {
-    if(!userSettings)
-        userSettings = defaultSettings;
+function init(settings) {
+    if(!settings)
+        return Promise.reject(new Error("pap.init: Invalid settings"));
+
+    var papSettings = settings.pap;
+
     return new Promise(function(resolve, reject) {
-        if(!userSettings.server) {
-            Storage.init(userSettings.storage, false).then(function() {
-                api.init(userSettings, Storage);
+        if(!papSettings.server) {
+            Storage.init(papSettings.storage, false).then(function() {
+                api.init(settings, Storage);
                 
                 // we are done - PAP is running locally without
                 // any REST interface
                 resolve();
             }, function(e) {
-                reject("ERROR: Unable to communicate to policy store. "+e);
+                reject("PAP is unable to communicate to policy store. "+e);
             });
         } else {
             var Server = require('./server');
             var Rest = require('./rest');
             
-            server = new Server(userSettings.server);
-            server.init(getServerInit(userSettings, server, Rest)).then(function(workers) {
+            server = new Server(papSettings.server);
+            server.init(getServerInit(settings, server, Rest)).then(function(workers) {
                 if(workers) {
-                    console.log("PAP Cluster is ready to receive requests.");
+                    module.exports.get = disabledAPI;
+                    module.exports.set = disabledAPI;
+                    module.exports.set = disabledAPI;
+                    
+                    w.info("PAP cluster is ready to receive requests.");
                     resolve();
                 }
             }, function(e) {
@@ -94,7 +84,9 @@ module.exports = {
     set: api.set,
     del: api.del,
 
-    getFullRecord: api.getFullRecord,
+    getFullRecord: api.getRecord,
+    getRecord: api.getRecord,
+    delRecord: api.delRecord,
 
     get app() { if(server) return server.app; else return null; }
 };
